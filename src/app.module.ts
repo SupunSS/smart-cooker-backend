038d911a08f648e367b1d2d633c8b4ad4365747b
SupunSS <93522604+SupunSS@users.noreply.github.com>
2024-11-04 16:01:14 +0530

    updated code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MqttService } from './mqtt/mqtt.service';
+import { OrderModule } from './order/order.module';
+import { DeviceModule } from './device/device.module';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true, // Makes ConfigModule available globally without re-importing
-  }),],
+  }), OrderModule, DeviceModule, UserModule, DatabaseModule,],
   providers: [MqttService,AppService],
   controllers: [AppController],
 })
